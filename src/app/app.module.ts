8319c97ebb61eb1df88d075759b1376d843633d4
alesandrocarvalho <alesandrocarvalho@salvador.ba.gov.br>
2019-07-15 11:13:23 -0300

    primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { MaterializeModule } from 'angular2-materialize';
+
 @NgModule({
   declarations: [
     AppComponent
